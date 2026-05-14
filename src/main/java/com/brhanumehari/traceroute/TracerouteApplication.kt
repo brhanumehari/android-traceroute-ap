760b08d8f7b89d82114531990d25e7c3f0113b2d
brhanumehari <101104691+brhanumehari@users.noreply.github.com>
2026-05-14 15:00:41 +0300

    Add application class for Hilt

---
+++ src/main/java/com/brhanumehari/traceroute/TracerouteApplication.kt
@@ -0,0 +1,10 @@
+package com.brhanumehari.traceroute
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Application class for Hilt dependency injection initialization
+ */
+@HiltAndroidApp
+class TracerouteApplication : Application()
